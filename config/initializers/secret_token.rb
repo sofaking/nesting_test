59cf63ea01a65d9dab2f83befeb45b64b36867ff
Anton Petrov <givemeletter@gmail.com>
2013-10-02 10:17:03 +0200

    Initial, but project scaffold is already here

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NestingTest::Application.config.secret_key_base = '641d7295199b3f25cb3a3e353b447bcf611f21e4baaebbdaceda81ca0d9325c3fd2f6707a7537d2aabe5960adb6dea52ab5839942331d7df4551cb990dc9fc84'
